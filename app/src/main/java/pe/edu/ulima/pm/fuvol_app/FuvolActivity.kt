4c0d15b6e05f47d0f0d0eb6ed3ffac73edbad87e
Fabrix300 <fabrix3434@gmail.com>
2021-06-22 13:01:19 -0500

    Pregunta 1 vista terminada

---
+++ app/src/main/java/pe/edu/ulima/pm/fuvol_app/FuvolActivity.kt
@@ -0,0 +1,11 @@
+package pe.edu.ulima.pm.fuvol_app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FuvolActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fuvol)
+    }
+}
